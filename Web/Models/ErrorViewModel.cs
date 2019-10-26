c732a5c45ac4fd8dc4bfb0f1a2406e54e4ae42b8
Akash Karve <akash.karve@nthrive.com>
2019-10-26 18:58:17 +0530

    project setip

---
+++ Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TimeSheetReconciliation.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
